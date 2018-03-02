4bfbab5d2a4bbccef2d0d76ae67d49132a746529
张帅男 <zhangshuainan@tbkt.cn>
2018-03-02 11:47:37 +0800

    zsn

---
+++ docker-test.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+
+"""
+@varsion: ??
+@author: 张帅男
+@file: docker-test.py
+@time: 2018/3/2 10:17
+"""
+
+import docker
+
+d = docker.Client(base_url="192.168.42.133:2375", version='auto', timeout=20)
+print d.version()    # 查看docker的版本
+print d.images()    # 查看docker 所有的镜像
+old_test = d.containers(all=True, filters={'name': "test"})         # 遍历所有容器得到当前镜像的状态
+old_test_1 = d.inspect_container("test")                            # 得到当前容器的状态
+print old_test_1['State']['Status']
+if old_test_1:
+    print old_test_1['State']['Status']                             # 得到该容器目前的状态
+    d.remove_container("test")    # 删除某个容器
+    d.start("test")                 # 运行某个容器
+    d.stop("test")                  # 停止某个容器
+else:
+    d.create_container("15294627382/baseos:1.0", command="/bin/sh", name="test")    # 创建容器
+
+
